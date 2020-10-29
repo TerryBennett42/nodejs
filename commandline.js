f9c34a696ece463b3a3fea8e0ab658b6db4d2af4
unknown <test@test.com>
2020-10-29 10:55:28 +0000

    creates

---
+++ commandline.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let greeting = 'Hello';
+let name = process.argv[2];
+
+console.log(greeting, 'Terry', name);
+
+console.log(process.argv.length);
+console.log(process.argv[0]);
+console.log(process.argv[1]);
+console.log(process.argv[2]);
+
---
+++ declaringvariables.js
@@ -0,0 +1,24 @@
+'use scrict';
+
+// declare variable message and assign initial value
+let message = 'Hello world';
+
+// print message
+console.log(message);
+
+// assigne new vale to the variable massage
+message = 'How are you';
+
+console.log(message);
+
+
+/*
+Multi-line comments
+asdasd
+asdasd
+asd
+asd
+asd
+as
+*/
+
